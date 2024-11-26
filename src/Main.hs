a392eba91cf08adb2e91778b48058ded7ee952e1
spikeyarmaku <spikeyarmaku@gmail.com>
2024-11-26 17:30:40 +0100

    Add application-based expr representation

--- src/Main.hs
+++ src/Main.hs
@@ -1,8 +1,20 @@
 module Main where
 
 import Tree
+import System.Environment
+
+helpMessage :: String -> String
+helpMessage progName =
+    "USAGE: " ++ progName ++ " input_file [-cX [output_file]] [-h]\n\n" ++
+    "FLAGS:\n" ++
+    "-cX output_file     Compile using backend X. Possible values for X:\n" ++
+    "                    I - interaction net backend\n" ++
+    "                    B - bitcoded tree backend\n" ++
+    "-h                  Display this message\n"
 
 main :: IO ()
-main = print $ parse "t(tt)(tt)"
+main = do
+    args <- getArgs
+    undefined
 
 
--- src/Tree.hs
+++ src/Tree.hs
@@ -1,7 +1,7 @@
 module Tree where
 
 import Data.Char (isSpace)
-import Control.Monad (join)
+import Data.Word (Word8)
 
 newtype Tree = Tree [Tree] deriving (Show, Eq)
 
@@ -9,15 +9,64 @@ printTree :: Tree -> String
 printTree (Tree []) = "t"
 printTree (Tree ts) = "(t" ++ concatMap printTree ts ++ ")"
 
+treeToExpr :: Tree -> Expr
+treeToExpr (Tree cs) = foldl applyExpr (Prg Leaf) (map treeToExpr cs)
+
+applyExpr :: Expr -> Expr -> Expr
+applyExpr (Prg p0) (Prg p1) = applyProgram p0 p1
+applyExpr (Prg p0) (App a0 a1) = App (Prg p0) (App a0 a1)
+applyExpr (App a0 a1) (Prg p0) = App (App a0 a1) (Prg p0)
+applyExpr (App a0 a1) (App a2 a3) = App (App (App a0 a1) a2) a3
+
+applyProgram :: Program -> Program -> Expr
+applyProgram (Fork p0 p1) p = App (Prg (Fork p0 p1)) (Prg p)
+applyProgram (Stem p0) p = Prg (Fork p0 p)
+applyProgram Leaf p = Prg (Stem p)
+
+isProgram :: Expr -> Bool
+isProgram (Prg _) = True
+isProgram _ = False
+
+getProgram :: Expr -> Maybe Program
+getProgram (Prg prg) = Just prg
+getProgram _ = Nothing
+
+printComb :: Expr -> String
+printComb = concatMap show . exprToComb
+
+exprToComb :: Expr -> [Comb]
+exprToComb (App e0 e1) = A : (exprToComb e0 ++ exprToComb e1)
+exprToComb (Prg Leaf) = [L]
+exprToComb (Prg (Stem p)) = S:exprToComb (Prg p)
+exprToComb (Prg (Fork p0 p1)) = F:(exprToComb (Prg p0) ++ exprToComb (Prg p1))
+
+data Comb = L | S | F | A deriving (Show, Enum, Eq)
+
+exprToBytes :: Expr -> [Word8]
+exprToBytes expr =
+    let combs = exprToComb expr
+        go [] = []
+        go cs =
+            let byte = sum
+                     $ zipWith (*) (map fromEnum $ take 4 cs)
+                               (map ((4 :: Int) ^) (reverse [0..3] :: [Int]))
+            in  fromIntegral byte : go (drop 4 cs)
+    in  go combs
+
 data Program
-    = F Program Program
-    | S Program
-    | L
+    = Fork Program Program
+    | Stem Program
+    | Leaf
     deriving (Show, Eq)
 
+-- Expr's are not allowed as part of Programs, because in the memory structure,
+-- applications are parts of a tree that can change, and the rest of the tree
+-- should be immutable. If there was an A node where a program is expected, that
+-- A node would eventually change the whole subtree, as A nodes are where
+-- reductions happen.
 data Expr
-    = A Tree Tree
-    | P Program
+    = App Expr Expr
+    | Prg Program
     deriving (Show, Eq)
 
 parse :: String -> Maybe Tree
@@ -43,6 +92,15 @@ apply (Just (Tree t1)) (Just (Tree t2)) = Just $ Tree $ t1 ++ [Tree t2]
 size :: Tree -> Int
 size (Tree cs) = 1 + sum (map size cs)
 
+----------
+-- TEST --
+----------
+
+-- Check if printing a parsed tree yields the same result as parsing and
+-- printing *that* tree
+-- (i.e. print(parse(text)) == print(parse(print(parse(text)))))
+-- The original text is not checked against, as the parentheses can mess it up
+-- and it is hard to get it right
 parseTest :: String -> Bool
 parseTest text
     | text == "" = True
