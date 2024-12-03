6adbe662d2e359bf24c333df1fd89f979c650a33
spikeyarmaku <spikeyarmaku@gmail.com>
2024-12-03 01:29:28 +0100

    Fix applyExpr, add expr printing and conversion to tree

--- src/Tree.hs
+++ src/Tree.hs
@@ -9,14 +9,26 @@ printTree :: Tree -> String
 printTree (Tree []) = "t"
 printTree (Tree ts) = "(t" ++ concatMap printTree ts ++ ")"
 
+printExpr :: Expr -> String
+printExpr = printTree . exprToTree
+
 treeToExpr :: Tree -> Expr
 treeToExpr (Tree cs) = foldl applyExpr (Prg Leaf) (map treeToExpr cs)
+
+exprToTree :: Expr -> Tree
+exprToTree (Prg Leaf) = Tree []
+exprToTree (Prg (Stem a)) = Tree [exprToTree (Prg a)]
+exprToTree (Prg (Fork a b)) = Tree [exprToTree (Prg a), exprToTree (Prg b)]
+exprToTree (App a0 a1) =
+    let (Tree tree0) = exprToTree a0
+        tree1 = exprToTree a1
+    in  Tree (tree0 ++ [tree1])
 
 applyExpr :: Expr -> Expr -> Expr
 applyExpr (Prg p0) (Prg p1) = applyProgram p0 p1
 applyExpr (Prg p0) (App a0 a1) = App (Prg p0) (App a0 a1)
 applyExpr (App a0 a1) (Prg p0) = App (App a0 a1) (Prg p0)
-applyExpr (App a0 a1) (App a2 a3) = App (App (App a0 a1) a2) a3
+applyExpr (App a0 a1) (App a2 a3) = App (App a0 a1) (App a2 a3)
 
 applyProgram :: Program -> Program -> Expr
 applyProgram (Fork p0 p1) p = App (Prg (Fork p0 p1)) (Prg p)
@@ -40,7 +52,7 @@ exprToComb (Prg Leaf) = [L]
 exprToComb (Prg (Stem p)) = S:exprToComb (Prg p)
 exprToComb (Prg (Fork p0 p1)) = F:(exprToComb (Prg p0) ++ exprToComb (Prg p1))
 
-data Comb = L | S | F | A deriving (Show, Enum, Eq)
+data Comb = L | S | F | A deriving (Show, Enum, Bounded, Eq)
 
 exprToBytes :: Expr -> [Word8]
 exprToBytes expr =
@@ -73,7 +85,8 @@ parse :: String -> Maybe Tree
 parse = fst . go [] Nothing
     where
     go :: [Maybe Tree] -> Maybe Tree -> String -> (Maybe Tree, String)
-    go _ expr "" = (expr, "")
+    go [] expr "" = (expr, "")
+    go _ _ "" = (Nothing, "")
     go stack expr (c:cs)
         | c == '(' = go (expr:stack) Nothing cs
         | c == ')' =
@@ -92,6 +105,17 @@ apply (Just (Tree t1)) (Just (Tree t2)) = Just $ Tree $ t1 ++ [Tree t2]
 size :: Tree -> Int
 size (Tree cs) = 1 + sum (map size cs)
 
+-- Count the number of leaves, stems, forks and applications
+stats :: [Comb] -> [(Comb, Int)]
+stats = go (zip [minBound..] (repeat 0))
+    where
+    go :: [(Comb, Int)] -> [Comb] -> [(Comb, Int)]
+    go s [] = s
+    go s (c:cs) =
+        let newS = map (\(comb, count) ->
+                if comb == c then (comb, count + 1) else (comb, count)) s
+        in  go newS cs
+
 ----------
 -- TEST --
 ----------
@@ -102,17 +126,22 @@ size (Tree cs) = 1 + sum (map size cs)
 -- The original text is not checked against, as the parentheses can mess it up
 -- and it is hard to get it right
 parseTest :: String -> Bool
-parseTest text
-    | text == "" = True
-    | all isSpace text = True
-    | otherwise =
-        let tree1 = parse text
-            tree2 = tree1 >>= parse . printTree
-            countNodes "" = 0 :: Int
-            countNodes (c:cs)
-                | c == '(' || c == ')' || isSpace c = countNodes cs
-                | otherwise = 1 + countNodes cs
-        in
-            case (tree1, tree2) of
-                (Just t1, Just t2) -> printTree t1 == printTree t2
-                _ -> countNodes text == 0
+parseTest text =
+    case parse text of
+        Nothing -> True
+        Just t0 ->
+            case parse (printTree t0) of
+                Nothing -> False
+                Just t1 -> printTree t0 == printTree t1
+
+-- Check if printing a parsed expr is the same as parsing and printing *that*
+-- text
+printTest :: String -> Bool
+printTest text =
+    case parse text of
+        Nothing -> True
+        Just t0 ->
+            case parse (printExpr . treeToExpr $ t0) of
+                Nothing -> False
+                Just t1 ->
+                    printExpr (treeToExpr t0) == printExpr (treeToExpr t1)
