42bdbfe33b67de05128a1bc20ff3576e4ae3d607
spikeyarmaku <spikeyarmaku@gmail.com>
2025-11-09 21:27:09 +0100

    Add vm tests

---
+++ src/bintree/test/vm.c
@@ -0,0 +1,146 @@
+#include "test.h"
+
+#include "../vm.h"
+#include <ctype.h>
+#include <string.h>
+
+size_t parse_expr(struct Tree* tree, const char **p);
+
+// Parse a single atom: either 't' or '(' expr ')'
+size_t parse_atom(struct Tree* tree, const char **p) {
+    while (isspace((unsigned char)**p)) {
+        (*p)++;
+    }
+
+    if (**p == 't') {
+        (*p)++;
+        return 0; // leaf
+    } else if (**p == '(') {
+        (*p)++;
+        int idx = parse_expr(tree, p);
+        if (**p != ')') {
+            fprintf(stderr, "Error: expected ')'\n");
+            exit(EXIT_FAILURE);
+        }
+        (*p)++;
+        return idx;
+    } else {
+        fprintf(stderr, "Unexpected char: %c\n", **p);
+        exit(EXIT_FAILURE);
+    }
+}
+
+// Parse left-associative applications
+size_t parse_expr(struct Tree* tree, const char **p) {
+    size_t left = parse_atom(tree, p);
+    while (1) {
+        const char* save = *p;
+        while (isspace((unsigned char)**p)) (*p)++;
+        if (**p == 't' || **p == '(') {
+            size_t right = parse_atom(tree, p);
+            left = tree_add_node(tree, App, left, right);
+        } else {
+            *p = save;
+            break;
+        }
+    }
+    return left;
+}
+
+struct VM vm_parse(const char* str) {
+    struct Tree tree = tree_make();
+    tree_add_node(&tree, 0, 0, 0);
+    size_t last_index = parse_expr(&tree, &str);
+    Node node = node_make_empty();
+    tree_set_node(&tree, 0, tree_get_node(tree, last_index));
+    tree_set_node(&tree, last_index, node_make_empty());
+    struct VM vm = vm_make(tree);
+    return vm;
+}
+
+void check_eval(const char* src, const char* expected) {
+    struct VM vm = vm_parse(src);
+    vm_run(&vm);
+    char buffer[65536];
+    tree_print(vm.tree, buffer, TRUE);
+    bool_t result = (strcmp(buffer, expected) == 0) ? TRUE : FALSE;
+    if (result == FALSE) {
+        printf("Result: %s, expected: %s\n", buffer, expected);
+        tree_debug_print(vm.tree);
+    }
+    sprintf(buffer, "%s -> %s", src, expected);
+    check(buffer, result);
+}
+
+void vm_test() {
+    printf("Simple trees\n");
+    check_eval("t",     "t");
+    check_eval("tt",    "tt");
+    check_eval("ttt",   "ttt");
+
+    printf("Reduction rules\n");
+    check_eval("tttt",          "t");
+    check_eval("t(tt)tt",       "tt(tt)");
+    check_eval("t(ttt)tt",      "t");
+    check_eval("t(ttt)t(tt)",   "tt");
+    check_eval("t(ttt)t(ttt)",  "ttt");
+
+    printf("Redexes on lower levels\n");
+    check_eval("t(tt(tttt))tt", "t");
+    check_eval("t(tt(tttt))t",  "t(ttt)t");
+
+    printf("All reduction rules in lower levels\n");
+    check_eval("t(tttt)",                       "tt");
+    check_eval("t(t(tt)tt)",                    "t(tt(tt))");
+    check_eval("t(t(ttt)tt)",                   "tt");
+    check_eval("t(t(ttt)t(tt))",                "t(tt)");
+    check_eval("t(t(ttt)t(ttt))",               "t(ttt)");
+    check_eval("t(tttt)(t(ttt)tt)(t(tt)tt)",    "t");
+    check_eval("t(t(tttt)(tttt)(tttt))(t(tttt)(tttt)(tttt))(t(tttt)(tttt)(tttt))",
+        "t");
+    check_eval("t (tttt) (t(tt)tt) (t(ttt)tt) (t(ttt)t(tt)) (t(ttt)t(ttt))",
+        "tt(ttt)");
+    check_eval("t (t (tttt) (t(tt)tt) (t(ttt)tt) (t(ttt)t(tt)) (t(ttt)t(ttt)))",
+        "t(tt(ttt))");
+
+    printf("All rules chained after each other (3, 2, 2, 5, 2, 4, 1)\n");
+    check_eval("t(t(ttt)tt)(t(tt)t(t(t(ttt)))tt)t", "tt");
+
+    printf("All rules, smaller version (2, 2, 2, 2, 5, 4, 2, 1, 3)\n");
+    check_eval("t (t (t (t (t (t t) t)))) t (t (t t t)) t", "tt");
+
+    printf("All rules chained after each other exactly once (2, 5, 4, 3, 1)\n");
+    check_eval("tt(t(t(t(ttt)))(tt)(t(ttt))tt)t", "t");
+
+    printf("Some random trees\n");
+    check_eval("t (tttt) (tttt) (tttt)",    "t");
+    check_eval("tt (ttt (tt)) t (ttt)" ,    "t(ttt)");
+    check_eval("t(tttt)t"              ,    "ttt");
+    check_eval("tt(tttt)"              ,    "ttt");
+    check_eval("t (tt (tttt)) t (tttt)",    "t");
+
+    printf("Test if children are reduced properly\n");
+    check_eval("ttttttt", "t");
+
+    printf("And True False\n");
+    check_eval("(t (t (t t t) (t t (t (t (t t)) t))) t) (t t) t", "t");
+
+    printf("And True True\n");
+    check_eval("(t (t (t t t) (t t (t (t (t t)) t))) t) (t t) (t t)", "tt");
+
+    printf("Not (And True False)\n");
+    check_eval("(t (t (t t) (t t t)) t) ((t (t (t t t) (t t (t (t (t t)) t))) t) (t t) t)",
+        "tt");
+
+    printf("Equal t t\n");
+    check_eval("(t (t (t (t (t t (t (t (t (t (t (t (t t)) t)) (t (t (t t)) t)))))) (t t))) (t t (t (t (t t (t (t (t (t (t t (t (t (t t t)) (t (t (t (t t) (t t t)) (t t (t t t))))))) (t (t (t (t (t t (t (t (t (t (t t (t (t (t t t)) t))) (t (t (t t (t t))) (t (t (t t t)) t)))) (t t (t t))))) (t (t (t t (t (t (t t t)) (t t))))))) (t t (t t (t t t)))))) (t (t (t t (t (t (t t (t (t (t t (t (t t (t t t))))))))))) (t (t (t (t (t t (t (t (t (t (t t (t (t (t t t)) t))) (t (t (t t (t t))) (t (t (t t t)) t)))) (t t (t t))))) (t (t (t t (t (t (t t (t (t (t t (t (t (t t t)) t))) (t t))))))) (t (t (t t (t (t (t t (t (t (t (t (t t (t (t (t t t)) t))) (t (t (t t (t t))) (t (t (t t t)) t)))) (t t (t t)))))))) (t (t (t t (t (t (t t (t (t (t t (t (t (t t (t (t (t t t)) t))) (t t)))))))))) (t (t (t t (t (t (t t (t (t (t t t) (t t (t (t (t t)) t))) t)))))))))))) (t (t (t t)) t)))))) (t (t (t t (t (t (t (t (t t (t (t (t (t (t (t (t t)) t)) (t (t (t t)) t)))))) (t t)))))) (t t))))) t t",
+        "tt");
+
+    printf("double 2\n");
+    check_eval("(t (t (t (t (t t (t (t t t)))) (t (t (t t (t t))) (t (t (t t (t t))) (t t))))) (t (t (t t)) t)) (t t (t (t t) t))",
+        "tt(tt(t(tt)t))");
+
+    printf("A collection of trees that didn't reduce correctly in earlier versions\n");
+    check_eval("t(t(t(tt)))t(ttt)", "t(t(ttt))t");
+    check_eval("t(t(ttt)tt)(tt)",   "tt(tt)");
+}
